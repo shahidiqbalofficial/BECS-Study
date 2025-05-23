26a9c138c0694e35465dd110c4ca24f559a2fe80
Shahid Iqbal <shahidiqbalofficial.is@gmail.com>
2025-05-23 12:22:31 +0500

    Fix: added nested repos as normal folders

---
+++ sem-08/TOS/class activty jankein/MyDeatail.java
@@ -0,0 +1,11 @@
+public class MyDetails {
+    public static void main(String[] args) {
+        String name = "shahid iqbal";
+        int age = 21;
+        String location = "attock";
+        
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Location: " + location);
+    }
+}
---
+++ sem-08/TOS/class activty jankein/readme.txt
@@ -0,0 +1,1 @@
+MyDeatail.java	readme.txt
